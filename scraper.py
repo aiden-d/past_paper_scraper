8db0f9f0e87b0999a9b3a76bf69a24a8b336bfb2
aiden-d <aidendawes@gmail.com>
2021-12-31 16:56:05 +0200

    Fixed some problems

--- scraper.py
+++ scraper.py
@@ -13,9 +13,24 @@ from pdfminer.pdfinterp import resolve1
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
-
-
+import os
+fileName = 'sample_math.pdf'
 listOfData = []
+
+
+# Get image dimensions and scale max x, y accordingly
+doc = fitz.open(fileName)
+page = doc.load_page(2)
+ir = fitz.IRect(0, 0, 9999, 9999)
+# TODO problem is that the different papers have different max coordinates, so we need to scale the max coordinates based on a max picture dimensions.
+pix = page.get_pixmap(dpi=300, clip=ir)
+output = "sample" + ".png"
+pix.save(output)
+image = Image.open('sample.png')
+width, height = image.size
+maxWidthUnits = (width/2677)*643
+maxHeightUnits = (height/3704)*890
+os.remove('sample.png')
 
 
 def get_text_coordinates(o: Any, depth=0) -> list:
@@ -57,7 +72,6 @@ def get_optional_text(o: Any) -> str:
     return ''
 
 
-fileName = 'sample_math.pdf'
 # pageNum = 4
 # text = convert_pdf_to_txt(fileName, pageNum)
 # print(text)
@@ -70,7 +84,6 @@ pagesData = {}
 
 
 def generate_data():
-    qNum = 0
     for i in range(2, pdfLen + 1):
         global listOfData
         listOfData = []
@@ -89,7 +102,7 @@ questionDataBank = {
     # question number: question number, coordinates, page number
 }
 
-# print(pagesData[4])
+print(pagesData[3])
 
 maxQuestionNum = 0
 
@@ -100,7 +113,23 @@ def find_questions():
     for i in range(2, pdfLen + 1):
         for d in pagesData[i]:
             questionStr = str(questionNumCount) + "."
+            questionStrOp2 = str(questionNumCount+1) + "."
+            questionStrOp3 = str(questionNumCount+2) + "."
             if (d[0] == questionStr):
+                questionDataBank[questionNumCount] = [
+                    questionNumCount, d[1], i]
+                if (questionNumCount > maxQuestionNum):
+                    maxQuestionNum = questionNumCount
+                questionNumCount += 1
+            elif (d[0] == questionStrOp2):
+                questionNumCount += 1
+                questionDataBank[questionNumCount] = [
+                    questionNumCount, d[1], i]
+                if (questionNumCount > maxQuestionNum):
+                    maxQuestionNum = questionNumCount
+                questionNumCount += 1
+            elif (d[0] == questionStrOp3):
+                questionNumCount += 2
                 questionDataBank[questionNumCount] = [
                     questionNumCount, d[1], i]
                 if (questionNumCount > maxQuestionNum):
@@ -113,10 +142,11 @@ print(questionDataBank)
 
 
 def pos_to_IRect(posarr) -> IRect:
+    global maxHeightUnits
     newx1 = posarr[0]
-    newy1 = 890 - posarr[1]
+    newy1 = maxHeightUnits - posarr[1]
     newx2 = posarr[2]
-    newy2 = 890 - posarr[3]
+    newy2 = maxHeightUnits - posarr[3]
     if (newx1 > newx2):
         temp = newx2
         newx2 = newx1
@@ -135,24 +165,39 @@ doc = fitz.open(fileName)
 def generate_images():
     global doc, questionDataBank, maxQuestionNum
     for i in range(1, maxQuestionNum+1):
-        q = questionDataBank[i]
-        nextq = None
-        if (i+1 <= maxQuestionNum):
-            nextq = questionDataBank[i+1]
+        if (i in questionDataBank):
+            q = questionDataBank[i]
 
-        page = doc.load_page(q[2])
-        # Width of page = 643, height of page = 890 units
-        c = q[1]
-        y1 = 100
-        if (nextq != None and q[2] == nextq[2]):
-            y1 = nextq[1][3] + 10
+            if (i+1 <= maxQuestionNum and i+1 in questionDataBank):
+                nextq = None
+                nextq = questionDataBank[i+1]
+                page = doc.load_page(q[2])
+                # Width of page = 643, height of page = 890 units
+                c = q[1]
+                y1 = 100
+                if (nextq != None and q[2] == nextq[2]):
+                    y1 = nextq[1][3] + 10
 
-        y2 = c[3]
-        ir = pos_to_IRect([0, y1, 590, y2+20])
+                y2 = c[3]
 
-        pix = page.get_pixmap(dpi=300, clip=ir)
-        output = "question"+str(q[0]) + ".png"
-        pix.save(output)
+                ir = pos_to_IRect([0, y1, 590, y2+20])
+
+                pix = page.get_pixmap(dpi=300, clip=ir)
+                output = "question"+str(q[0]) + ".png"
+                pix.save(output)
+            else:
+                page = doc.load_page(q[2])
+                # Width of page = 643, height of page = 890 units
+                c = q[1]
+                y1 = 100
+
+                y2 = c[3]
+
+                ir = pos_to_IRect([0, y1, 590, y2+20])
+
+                pix = page.get_pixmap(dpi=300, clip=ir)
+                output = "question"+str(q[0]) + ".png"
+                pix.save(output)
 
 
 generate_images()
